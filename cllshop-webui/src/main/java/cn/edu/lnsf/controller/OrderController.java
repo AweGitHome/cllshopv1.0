f2d36f3fc500bf4a59b852803353b30d75060420
AweGitHome <1343685578@qq.com>
2019-06-17 18:42:35 +0800

    订单查看

---
+++ cllshop-webui/src/main/java/cn/edu/lnsf/controller/OrderController.java
@@ -0,0 +1,35 @@
+package cn.edu.lnsf.controller;
+
+import cn.edu.lnsf.entity.Order;
+import cn.edu.lnsf.entity.OrdersProduct;
+import cn.edu.lnsf.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class OrderController {
+    private OrderService orderService;
+
+    @Autowired
+    public void setOrderService(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @RequestMapping("orders.html")
+    String getAllByUid(Integer uid, HttpServletRequest request){
+        List<Order> orders = orderService.getAllOrdersByUid(uid);
+        request.setAttribute("orders",orders);
+        return "forward:/showorder.jsp";
+    }
+
+    @RequestMapping("orderDetails.html")
+    String showDetails(Integer oid, HttpServletRequest request){
+        List<OrdersProduct> orderDetails = orderService.getOrderDetailByOid(oid);
+        request.setAttribute("orderDetails",orderDetails);
+        return "forward:/orderDetails.jsp";
+    }
+}
